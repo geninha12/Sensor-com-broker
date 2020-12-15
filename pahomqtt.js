696c4e9999ceb7a490594efcd5a62747e8cce6a5
Maria Eugênia Lemos <geninhaa12@gmail.com>
2020-12-15 14:44:51 -0300

    Update pahomqtt.js

--- pahomqtt.js
+++ pahomqtt.js
@@ -35,6 +35,7 @@ function MQTTconnect() {
     //document.write("connecting to "+ host);
     var options = {
         timeout: 3,
+	    useSSL: true;
         onSuccess: onConnect,
         onFailure: onFailure,
     };
